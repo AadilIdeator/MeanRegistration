854c6815273c92c571e90c93ac500b80c1650c5e
AadilIdeator <adilkazi1997@gmail.com>
2019-12-21 07:46:42 -0800

    #firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TemplateFormComponent } from './template-form/template-form.component';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
+import { from } from 'rxjs';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import {approutes} from '../app/apps.routes';
+import { HomeComponent } from './home/home.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
